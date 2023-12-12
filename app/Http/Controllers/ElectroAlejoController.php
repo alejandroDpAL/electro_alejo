dc4c484a57d55cf6cbd695c667e49ef9a2d4f7fd
alejandroDpAL <alejandropasaje799@gmail.com>
2023-12-12 14:25:41 -0500

    add

---
+++ app/Http/Controllers/ElectroAlejoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\electro_alejo;
+use Illuminate\Http\Request;
+
+class ElectroAlejoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(electro_alejo $electro_alejo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(electro_alejo $electro_alejo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, electro_alejo $electro_alejo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(electro_alejo $electro_alejo)
+    {
+        //
+    }
+}
